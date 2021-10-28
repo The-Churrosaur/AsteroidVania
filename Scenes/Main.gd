a257c18c2d118f51cb75fb5849860ece7adcb1b1
Daichi <daichi@murokami.com>
2021-10-28 05:08:24 -0700

    branching cleanup

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready():
-	get_tree().change_scene("res://Scenes/levels/TestScene2x.tscn")
+#	get_tree().change_scene("res://Scenes/levels/TestScene2x.tscn")
 #	get_tree().change_scene("res://Scenes/levels/shake_test.tscn")
 #	get_tree().change_scene("res://Scenes/levels/level_utils/ik_test.tscn")
-#	get_tree().change_scene("res://Scenes/levels/level_builder_test.tscn")
+	get_tree().change_scene("res://Scenes/levels/level_builder_test.tscn")
